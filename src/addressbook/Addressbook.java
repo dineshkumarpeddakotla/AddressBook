4b4f28e7caca91015e82c3ce12010d885ab72a73
dinnu247 <dineshkumar.icon@gmail.com>
2021-03-26 14:30:34 +0530

    Created Statement to Added New Contact Deatials in Addressbook

--- src/addressbook/Addressbook.java
+++ src/addressbook/Addressbook.java
@@ -1,9 +1,11 @@
 package addressbook;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Addressbook {
-
+    public static final int Add_details = 1;
     //Scanner is used to get the data from user
     Scanner user = new Scanner(System.in);
 //Declaring Variables
@@ -33,17 +35,27 @@ public class Addressbook {
     //Displaying the output in String Format
     public String toString() {
 
-        return ("FIRST NAME : " +First_Name+ "\nLAST NAME : " +Last_Name+ "\nAddress"+Address+"\nCity"+City+"\nState"+State+
-                "\nZipcode"+Zipcode+"\nPhone number"+PhoneNumber+"Email Id"+Email_Id);
+        return ("FIRST NAME : " +First_Name+ "\nLAST NAME : " +Last_Name+ "\nAddress : "+Address+"\nCity : "+City+"\nState : "+State+
+                "\nZipcode : "+Zipcode+"\nPhone number : "+PhoneNumber+"Email Id : "+Email_Id);
     }
     public static void main(String[] args ){
         System.out.println("Welome to Address book");
-        Addressbook person = new Addressbook();
-        System.out.println("GIVE ME Person DETAILS");
-        person.set_contact_details();
+        Scanner input = new Scanner(System.in);
+        //creating a user data list
+        List<Addressbook> user_data = new ArrayList<>();
+        System.out.println("HOW MANY USERS DO YOU WANT TO ADD");
+//asking a user to add details or not
+        int choice=input.nextInt();
+        if (choice == Add_details) {
 
-
-
-
+            int user_choice = input.nextInt();
+            for (int i = 0; i < user_choice; i++) {
+              //creating an object and adding given details into user data list
+                Addressbook Person = new Addressbook();
+                Person.set_contact_details();
+                user_data.add(Person);
+                System.out.println(Person.toString());
+            }
+        }
     }
 }
